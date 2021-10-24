3ca76ca92d2f88e209a4f75fdb3621592b1ef14d
michalfirszt <michalfiszu@gmail.com>
2021-10-24 12:16:39 +0200

    Message action type

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 
 dotenv.config();
 
@@ -7,4 +7,49 @@ const wsServer = new WebSocketServer({ port: process.env.WS_PORT });
 
 wsServer.on('connection', (ws) => {
   console.log(`New connection ${new Date()}`);
+
+  ws.on('message', (action) => {
+    const { type, payload } = JSON.parse(action);
+
+    switch (type) {
+      case 'message': {
+        wsServer.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                type: 'message',
+                payload,
+              })
+            );
+          }
+        });
+
+        break;
+      }
+      case 'typing': {
+        wsServer.clients.forEach((client) => {
+          if (client !== ws && client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                type: 'typing',
+                payload: {
+                  user: { name: payload.currentUser.name },
+                },
+              })
+            );
+          }
+        });
+
+        break;
+      }
+      default: {
+        console.log(`Unknown action type: ${type}`);
+        break;
+      }
+    }
+  });
+
+  ws.on('close', () => {
+    console.log(`Disconnect ${new Date()}`);
+  });
 });
