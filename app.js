ee82ca48235ffcc0c930095bdd66497c70226030
michalfirszt <michalfiszu@gmail.com>
2021-11-01 18:01:18 +0100

    Save new message

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import WebSocket, { WebSocketServer } from 'ws';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -10,20 +11,34 @@ wsServer.on('connection', (ws) => {
 
   ws.on('message', (action) => {
     const { type, payload } = JSON.parse(action);
+    let messageId;
 
     switch (type) {
       case 'message': {
+        messageId = Date.now();
+
         wsServer.clients.forEach((client) => {
           if (client.readyState === WebSocket.OPEN) {
             client.send(
               JSON.stringify({
                 type: 'message',
-                payload,
+                payload: {
+                  id: messageId,
+                  ...payload,
+                },
               })
             );
           }
         });
 
+        axios.post('http://localhost:4000/messages', {
+          id: messageId,
+          channelId: payload.channelId,
+          content: payload.content,
+          username: payload.username,
+        });
+
+        messageId = undefined;
         break;
       }
       case 'typing': {
